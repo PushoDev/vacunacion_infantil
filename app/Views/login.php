dd5d34e01e57da9fe526c27bd085fe1f2bbad814
Luis A. Guisado <bethocubans1990@gmail.com>
2023-12-01 17:56:02 +0100

    Update login.php
    Co-Autores
    
    Co-Authored-By: PuShoft <151240602+pushoft@users.noreply.github.com>
    Co-Authored-By: Luis A. Guisado <130678121+pushodev@users.noreply.github.com>

--- app/Views/login.php
+++ app/Views/login.php
@@ -28,7 +28,7 @@
 		</form>
 	</div>
 	<!--====== Scripts -->
-	<script src="./js/jquery-3.1.1.min.js"></script>
+	<script src="<?php echo base_url('/js/jquery-3.1.1.min.js') ?>"></script>	
 	<script src="./js/bootstrap.min.js"></script>
 	<script src="./js/material.min.js"></script>
 	<script src="./js/ripples.min.js"></script>
